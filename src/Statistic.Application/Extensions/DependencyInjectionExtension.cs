1bda7fbba3c8fd2ee264a766d8e5b175469dd25a
SteveBob <88212395+Athurito@users.noreply.github.com>
2024-02-15 18:05:37 +0100

    Add AddressService and VisitorService to DI setup
    The Dependency Injection (DI) setup in the Statistic Application has been updated. Transient lifetimes of `AddressService` and `VisitorService` have been added to the services collection to be used across the application.

--- src/Statistic.Application/Extensions/DependencyInjectionExtension.cs
+++ src/Statistic.Application/Extensions/DependencyInjectionExtension.cs
@@ -1,3 +1,5 @@
+using Statistic.Application.Services;
+
 namespace Statistic.Application.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -5,6 +7,9 @@ public static class DependencyInjectionExtension
 {
     public static IServiceCollection AddApplication(this IServiceCollection service)
     {
+        service.AddTransient<IAddressService, AddressService>();
+        service.AddTransient<IVisitorService, VisitorService>();
+        
         return service;
     }
 }
