40afeb308f14c19c2ebc858e6e1746474b631c5c
SteveBob <88212395+Athurito@users.noreply.github.com>
2024-02-16 01:46:41 +0100

    Add repositories to DependencyInjectionExtension
    The modification includes adding transient services for IVisitorRepository and IAddressRepository to the AddInfrastructure method in the DependencyInjectionExtension class. These additions ensure that the implementations of these repositories are appropriately injected when needed.

--- src/Statistic.Infrastructure/Extensions/DependencyInjectionExtension.cs
+++ src/Statistic.Infrastructure/Extensions/DependencyInjectionExtension.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Statistic.Infrastructure.Data;
 using Microsoft.Extensions.DependencyInjection;
+using Statistic.Domain.Repositories;
+using Statistic.Infrastructure.Repositories;
 
 namespace Statistic.Infrastructure.Extensions;
 
@@ -8,6 +10,9 @@ public static class DependencyInjectionExtension
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection service)
     {
+        service.AddTransient<IVisitorRepository, VisitorRepository>();
+        service.AddTransient<IAddressRepository, AddressRepository>();
+        
         service.AddDbContext<StatisticDbContext>(options=>
             options.UseMySQL("server=localhost;Uid=bayerles;pwd=geheim;database=statistic;allowuservariables=True;SslMode=None"));
         return service;
