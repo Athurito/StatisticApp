65f3d6d86132c6f730fde35b944f732d385c1fdd
SteveBob <88212395+Athurito@users.noreply.github.com>
2024-02-20 17:56:29 +0100

    Integrate database initialization at startup
    The database is now initialized upon application launch. The changes include adding a check to ensure the database has been created and a process for opening the main application window. These enhancements improve application stability by ensuring the database is ready before other operations are performed.

--- src/Statistic.Wpf/App.xaml.cs
+++ src/Statistic.Wpf/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Statistic.Application.Extensions;
+using Statistic.Infrastructure.Data;
 using Statistic.Infrastructure.Extensions;
 using Statistic.Wpf.Extensions;
 using Statistic.Wpf.View;
@@ -17,9 +19,22 @@ public partial class App : System.Windows.Application
     }
     protected override void OnStartup(StartupEventArgs e)
     {
+        EnsureDataBase();
+        OpenMainWindow();
+    }
+
+    private void OpenMainWindow()
+    {
         MainWindow = _serviceProvider!.GetRequiredService<StatisticWindow>();
         MainWindow.DataContext = _serviceProvider!.GetRequiredService<StatisticViewModel>();
         MainWindow.ShowDialog();
+    }
+
+    private void EnsureDataBase()
+    {
+        var context = _serviceProvider!.GetRequiredService<IDbContextFactory<StatisticDbContext>>();
+        var ctx = context.CreateDbContext();
+        ctx.Database.EnsureCreated();
     }
 
     private void RegisterServices()
